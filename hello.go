72f225480a7580fb9d106a02b8eea91746e25235
leandrorosa <leandro.rosagf@gmail.com>
2018-04-02 13:26:58 -0300

    aula 6

--- hello.go
+++ hello.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -57,10 +60,7 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	sites :=
-		[]string{"https://random-status-code.herokuapp.com/",
-			"https://www.alura.com.br",
-			"https://www.caelum.com.br"}
+	sites := leSitesDoArquivo()
 
 	for i := 0; i < monitoramentos; i++ {
 		for i, site := range sites {
@@ -71,6 +71,32 @@ func iniciarMonitoramento() {
 		fmt.Println("")
 	}
 	fmt.Println("")
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+	return sites
 }
 
 func testaSite(site string) {
